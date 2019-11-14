0c87d3850a1bbb73ae758fe40a613dc84f071fb0
dinotoju <44514196+dinotoju@users.noreply.github.com>
2019-11-14 17:01:57 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 [].forEach.call(document.querySelectorAll("a-box"), function(box) {
    box.addEventListener("mouseenter", function() {
-      this.setAttribute("color", "red");
+      this.setAttribute("color", "green");
    });
 
    box.addEventListener("mouseleave", function() {
       this.setAttribute("color", "white");
    });
 
-   box.addEventListener("click", function() {
-      this.setAttribute("color", "green");
-	  document.querySelector("#usd").setAttribute("visible", true);
+   box.addEventListener("mouseenter", function() {
+	  document.querySelector("#eur").setAttribute("visible", true);
+   });
+	
+	box.addEventListener("mouseleave", function() {
+       document.querySelector("#eur").setAttribute("visible", false);
    });
 });
